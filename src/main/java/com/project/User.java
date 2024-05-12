27eeb966260ab89b5347f98715da583debf88891
Najer-jeril <mnazrililham55@gmail.com>
2024-05-12 10:54:24 +0800

    class ada error

--- src/main/java/com/project/User.java
+++ src/main/java/com/project/User.java
@@ -2,7 +2,7 @@ package com.project;
 
 public class User extends Pengguna{
 
-    public User(char id, String name, String pass, "user"){
+    public User(char id, String name, String pass, 'user'){
 
     }
 }
