e640ef8c6dc5f817dda11d33f7d49d48a624d36c
Fadhil Awalia Kusuma <11231023@student.itk.ac.id>
2024-05-29 15:08:26 +0800

    Penambahan CSS untuk GUI

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -28,7 +28,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("view/primary"));
         stage.setTitle("Petrovate");
         stage.setResizable(false);
         stage.setScene(scene);
